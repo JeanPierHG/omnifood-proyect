e168766c702fe4ad6aa11b69cd4c22cf97ab66b4
unknown <jeanpier.dev@outlook.com>
2022-08-14 12:44:31 -0500

    finished project

--- js/script.js
+++ js/script.js
@@ -1,48 +1,60 @@
 function checkFlexGap() {
-  let flex = document.createElement('div');
-  flex.style.display = 'flex';
-  flex.style.flexDirection = 'column';
-  flex.style.rowGap = '1px';
+	let flex = document.createElement('div');
+	flex.style.display = 'flex';
+	flex.style.flexDirection = 'column';
+	flex.style.rowGap = '1px';
 
-  flex.appendChild(document.createElement('div'));
-  flex.appendChild(document.createElement('div'));
+	flex.appendChild(document.createElement('div'));
+	flex.appendChild(document.createElement('div'));
 
-  document.body.appendChild(flex);
-  let isSupported = flex.scrollHeight === 1;
-  flex.parentNode.removeChild(flex);
+	document.body.appendChild(flex);
+	let isSupported = flex.scrollHeight === 1;
+	flex.parentNode.removeChild(flex);
 
-  if (!isSupported) document.body.classList.add('no-flexbox-gap');
+	if (!isSupported) document.body.classList.add('no-flexbox-gap');
 }
 console.log('Hola');
 checkFlexGap();
 
+/*
+ *MENU NAVIGATION
+ */
 const header = document.querySelector('header');
 const btnMenu = document.querySelector('.btn-mobile-nav');
 
-console.log(btnMenu);
-
 btnMenu.addEventListener('click', () => {
-  header.classList.toggle('nav-open');
-  if (header.classList.contains('nav-open'))
-    document.querySelector('body').style.overflow = 'hidden';
+	header.classList.toggle('nav-open');
+	if (header.classList.contains('nav-open'))
+		document.querySelector('body').style.overflow = 'hidden';
+});
+
+/*
+ * LINKS MENU NAVIGATION
+ */
+const menulistLinkEl = document.querySelectorAll('.main-nav-list li');
+menulistLinkEl.forEach((link) => {
+	link.addEventListener('click', () => {
+		document.querySelector('body').style.overflow = 'auto';
+		header.classList.remove('nav-open');
+	});
 });
 
 /* STICKY NAVIGATION */
 const sectionHeroEl = document.querySelector('.section-hero');
 const observer = new IntersectionObserver(
-  (entries) => {
-    const ent = entries[0];
-    if (!ent.isIntersecting) {
-      document.querySelector('body').classList.add('sticky');
-    } else {
-      document.querySelector('body').classList.remove('sticky');
-    }
-  },
-  {
-    root: null,
-    threshold: 0,
-    rootMargin: '-80px',
-  }
+	(entries) => {
+		const ent = entries[0];
+		if (!ent.isIntersecting) {
+			document.querySelector('body').classList.add('sticky');
+		} else {
+			document.querySelector('body').classList.remove('sticky');
+		}
+	},
+	{
+		root: null,
+		threshold: 0,
+		rootMargin: '-80px',
+	}
 );
 
 observer.observe(sectionHeroEl);
